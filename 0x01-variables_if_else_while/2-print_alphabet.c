6a28a75fc5170efaf9c06a90d3da3a087121c0a5
hoyibs <osimeon206@gmail.com>
2022-09-09 12:42:09 -0700

    I sometimes suffer from insomnia. And when I can't fall asleep, I play what I call the alphabet game

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -1,60 +1,18 @@
-#include <stdlib.h>
-
-#include <time.h>
-
 #include <stdio.h>
-
-
-
+#include <stdlib.h>
+#include <time.h>
 /**
- *
- * * main - assign a random number to the variable n each time it is executed
- *
- * * The string Last digit of n
- * *
-* Return: 0
+* main - program that prints the alphabet in lowercase, followed by a new line
+* You can only use the putchar
+*
+* Return: Always 0 (Success)
 */
-
-
 int main(void)
-
 {
-
-int n;
-
-int lastn;
-
-
-srand(time(0));
-
-n = rand() - RAND_MAX / 2;
-
-lastn = n % 10;
-
-if (lastn > 5)
-
-{
-
-printf("Last digit of %d is %d and is greater than 5\n", n, lastn);
-
-}
-
-else if (lastn == 0)
-
-{
-
-printf("Last digit of %d is %d and is 0\n", n, lastn);
-
-}
-
-else if (lastn < 6 && lastn != 0)
-
-{
-
-printf("Last digit of %d is %d and is less than 6 and not 0\n", n, lastn);
-
-}
+char low;
+for (low = 'a'; low <= 'z'; low++)
+putchar(low);
+printf("\n");
 
 return (0);
-
 }
