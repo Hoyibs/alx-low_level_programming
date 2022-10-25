600f256c942a2b2cdf779c450b30b127a05b7c40
Simeon <105791527+Hoyibs@users.noreply.github.com>
2022-10-26 00:18:07 +0200

    Task 12. Print (safe version)

--- 0x13-more_singly_linked_lists/101-print_listint_safe.c
+++ 0x13-more_singly_linked_lists/101-print_listint_safe.c
@@ -1,36 +1,30 @@
 #include "lists.h"
 
+#include "lists.h"
+
 /**
- * print_listint_safe - Prints a listint_t list safely.
- * @head: A pointer to the head of the listint_t list.
+ * print_listint_safe - Listas enlazadas
+ * @head: Dado desde main
  *
- * Return: The number of nodes in the list.
+ * Return: i
  */
 size_t print_listint_safe(const listint_t *head)
 {
-	size_t nodes, index = 0;
+	size_t i = 0;
+	const listint_t *temp, *node;
 
-	nodes = looped_listint_len(head);
-
-	if (nodes == 0)
+	node = head;
+	while (node != NULL)
 	{
-		for (; head != NULL; nodes++)
+		printf("[%p] %d\n", (void *)node, node->n);
+		temp = node;
+		node = node->next;
+		i++;
+		if (temp <= node)
 		{
-			printf("[%p] %d\n", (void *)head, head->n);
-			head = head->next;
+			printf("-> [%p] %d\n", (void *)node, node->n);
+			break;
 		}
 	}
-
-	else
-	{
-		for (index = 0; index < nodes; index++)
-		{
-			printf("[%p] %d\n", (void *)head, head->n);
-			head = head->next;
-		}
-
-		printf("-> [%p] %d\n", (void *)head, head->n);
-	}
-
-	return (nodes);
+	return (i);
 }
